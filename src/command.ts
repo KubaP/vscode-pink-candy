752a87211f986665356f1b98fd196589c3edad83
KubaP <git@kubap.io>
2022-05-18 22:55:55 +0100

    refactor: Configuration key names
    - Rename the configuration keys so that they look better on the settings page.
    - Reword some of the descriptive text to flow better.
    - Rename `Style` to `InlayStyle` to avoid confusion.

--- src/command.ts
+++ src/command.ts
@@ -20,16 +20,16 @@ function toggleBoolean(name: string) {
 async function pickInlayStyle() {
     const config = vscode.workspace.getConfiguration("theme-pink-candy");
 
-    const style: string | undefined = config.get("inlayStyle");
+    const style: string | undefined = config.get("inlayHintStyle");
     if (style === undefined) {
-        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.inlayStyle'");
+        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.inlayHintStyle'");
         return;
     }
 
     // MAYBE: Replace this with the more advanced `window.createQuickPick` which allows setting the default
     // selected entry for example.
 
-    // Note: The cases should match the configuration keys in `package.json`/`config.Style`.
+    // Note: The cases should match the configuration keys in `package.json`/`config.InlayStyle`.
     let placeHolder;
     switch (style) {
         case "noBackground": placeHolder = "Currently selected: No background"; break;
@@ -38,28 +38,28 @@ async function pickInlayStyle() {
         default: placeHolder = "Currently selected: Invalid";
     }
 
-    // Note: The keys should match the configuration keys in `package.json`/`config.Style`.
+    // Note: The keys should match the configuration keys in `package.json`/`config.InlayStyle`.
     const options = [
         {
             label: "No background",
-            detail: "No background behind the inlay hint.",
+            detail: "No background behind the inlay hint; this is the 'default' option.",
             key: "noBackground",
         },
         {
             label: "Faint background",
-            detail: "A faint background behind the inlay hint.",
+            detail: "A lightly shaded background behind the inlay hint for better contrast.",
             key: "faintBackground",
         },
         {
             label: "Accent text",
-            detail: "The inlay hint text has an accent colour and a faint background.",
+            detail: "An accent colour for the inlay hint text and a shaded background.",
             key: "accent",
         }
     ];
 
     try {
         const result = await vscode.window.showQuickPick(options, {
-            title: "Select the inlay hint style",
+            title: "Select the appearance of inlay hints",
             canPickMany: false,
             placeHolder,
             onDidSelectItem: () => { }
@@ -70,23 +70,23 @@ async function pickInlayStyle() {
             return;
         }
 
-        config.update("inlayStyle", result.key, true);
+        config.update("inlayHintStyle", result.key, true);
     } catch (err) {
         console.error(err);
     }
 }
 
 const toggleMutedMdCmd = vscode.commands.registerCommand("theme-pink-candy.toggleMutedMarkdown", () => {
-    toggleBoolean("mutedMarkdown");
+    toggleBoolean("mutedMarkdownPlaintext");
 });
 const toggleItalicCommentsCmd = vscode.commands.registerCommand("theme-pink-candy.toggleItalicComments", () => {
-    toggleBoolean("italicComments");
+    toggleBoolean("italicizedComments");
 });
 const toggleAltCurrentLineCmd = vscode.commands.registerCommand("theme-pink-candy.toggleAltCurrentLine", () => {
-    toggleBoolean("altCurrentLine");
+    toggleBoolean("alternateCurrentLineStyle");
 });
 const toggleMonochromeBracketsCmd = vscode.commands.registerCommand("theme-pink-candy.toggleMonochromeBrackets", () => {
-    toggleBoolean("monochromeBracketGuides")
+    toggleBoolean("monochromeBracketPairGuides")
 });
 const pickInlayStyleCmd = vscode.commands.registerCommand("theme-pink-candy.pickInlayStyle", () => {
     pickInlayStyle();
--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ import { info } from "./extension";
 /**
  * Valid inlay hint styles.
  */
-type Style = "noBackground" | "faintBackground" | "accent";
-function isValidStyle(str: string): str is Style {
+type InlayStyle = "noBackground" | "faintBackground" | "accent";
+function isValidStyle(str: string): str is InlayStyle {
     return str == "noBackground" || str == "faintBackground" || str == "accent";
 }
 
@@ -17,9 +17,9 @@ export class Config {
     italicComments: boolean;
     altCurrentLine: boolean;
     monochromeBracketGuides: boolean;
-    inlayStyle: Style;
+    inlayStyle: InlayStyle;
 
-    constructor(mutedMd: boolean, italicComments: boolean, altCurrentLine: boolean, monochromeBracketGuides: boolean, inlayStyle: Style) {
+    constructor(mutedMd: boolean, italicComments: boolean, altCurrentLine: boolean, monochromeBracketGuides: boolean, inlayStyle: InlayStyle) {
         this.mutedMd = mutedMd;
         this.italicComments = italicComments;
         this.altCurrentLine = altCurrentLine;
@@ -34,40 +34,40 @@ export class Config {
 export function getConfig(): Config {
     const config = vscode.workspace.getConfiguration("theme-pink-candy");
 
-    let mutedMd: boolean | undefined = config.get("mutedMarkdown");
+    let mutedMd: boolean | undefined = config.get("mutedMarkdownPlaintext");
     if (mutedMd === undefined) {
-        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.mutedMarkdown'");
+        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.mutedMarkdownPlaintext'");
         mutedMd = false;
     }
 
-    let italicComments: boolean | undefined = config.get("italicComments");
+    let italicComments: boolean | undefined = config.get("italicizedComments");
     if (italicComments === undefined) {
-        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.italicComments'")
+        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.italicizedComments'")
         italicComments = false;
     }
 
-    let altCurrentLine: boolean | undefined = config.get("altCurrentLine");
+    let altCurrentLine: boolean | undefined = config.get("alternateCurrentLineStyle");
     if (altCurrentLine === undefined) {
-        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.altCurrentLine'")
+        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.alternateCurrentLineStyle'")
         altCurrentLine = false;
     }
 
-    let monochromeBracketGuides: boolean | undefined = config.get("monochromeBracketGuides");
+    let monochromeBracketGuides: boolean | undefined = config.get("monochromeBracketPairGuides");
     if (monochromeBracketGuides === undefined) {
-        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.monochromeBracketGuides'")
+        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.monochromeBracketPairGuides'")
         monochromeBracketGuides = false;
     }
 
-    let inlayStyle: Style;
-    let inlayStyleRaw: string | undefined = config.get("inlayStyle");
+    let inlayStyle: InlayStyle;
+    let inlayStyleRaw: string | undefined = config.get("inlayHintStyle");
     if (inlayStyleRaw === undefined) {
-        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.inlayStyle'")
+        info.appendLine("ERROR: Could not find value of 'theme-pink-candy.inlayHintStyle'")
         inlayStyleRaw = "noBackground";
     }
     if (isValidStyle(inlayStyleRaw)) {
         inlayStyle = inlayStyleRaw;
     } else {
-        info.appendLine(`ERROR: Invalid value '${inlayStyleRaw}' of 'theme-pink-candy.inlayStyle'`)
+        info.appendLine(`ERROR: Invalid value '${inlayStyleRaw}' of 'theme-pink-candy.inlayHintStyle'`)
         inlayStyle = "noBackground";
     }
 
