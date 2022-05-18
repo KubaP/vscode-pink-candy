643207a25da9fedda98ce184dbc1b449a0f33ad6
KubaP <git@kubap.io>
2022-05-18 19:06:08 +0100

    refactor: Move colour objects
    - Move the colour objects into their own files (mainly to improve performance with the colorizer extension).

---
+++ src/dark.ts
@@ -0,0 +1,383 @@
+export const darkColors = {
+
+    text: {
+        normal: "#ABB2BF",
+
+        // Text editor line numbers, suggestion ghost text, inactive tabs,
+        muted: "#636D83",
+
+        // Breadcrumbs, general descriptions, unfocused titlebar,
+        light: "#7C869B",
+
+        // Status bar foreground, intellisense matching letters, intellisense selected foreground, button text,
+        // list selected text, badge foreground, command bar selected foreground, menu selected text, banner text,
+        inverse: "#FFFFFF",
+
+        // Peek view title, peek view filenames,
+        emphasised: "#FFFFFF",
+
+        // Unnecessary text editor text,
+        faded: "#000000C0",
+
+        decoration: {
+            // Indentation guides, whitespace chars, rulers,
+            light: "#3D434F",
+            // Current indentation guide, matching bracket border,
+            dark: "#5A6375",
+            // Codelens
+            codelens: "#9C9C9C",
+        },
+
+        // Text editor current line, text editor folded line, hover over symbol background box,
+        currentLine: "#FFFFFF07",
+        currentLineBorder: "#FFFFFF10",
+
+        // General selection box background, input option, text editor selection boxes, terminal selection,
+        selectionBg: "#81577A",
+        selectionBgA: "#FF4C981A",
+        // Unfocused text editor selection boxes,
+        secondarySelectionBgA: "#FF4C9810",
+
+        // Tabstop box backgrounds,
+        tabstopBgA: "#FF4C981A",
+
+        // Text editor & terminal highlight boxes, line background, search sidebar,
+        // peek view match highlight boxes, list filter match highlight boxes,
+        matchBg: "#4E522B",
+        matchBgA: "#CCD00C3A",
+        matchBorder: "#31A155",
+        matchBorderA: "#31A1558A",
+    },
+
+    // Cursor, editor line number, breadcrumbs focus text/background, intellisense widget matching text,
+    // intellisense widget selected background, peek view selected background, focused borders, button background,
+    // checkbox tick, list selected background, progress bar, badge background, focused tab indicator,
+    // activity bar drop border, activity bar active icon, modified setting indicator,  active panel indicator, 
+    // command bar selected background, menu selected background, border drag, banner background,
+    // status bar background, terminal cursor,
+    primary: "#FF1277",
+    primaryHover: "#DA005F",
+
+    // Text editor, gutter, minimap, breadcrumbs, peek view text editor, empty editor group, panel, terminal,
+    // active tab, hover tab, zen mode sides, activity bar, welcome page, tutorial page, notifications,
+    // drag-and-drop prompt,
+    primaryBg: "#22252a",
+
+    diag: {
+        // Ruler/minimap decorations, underline squiggles, error lens foregrounds, problem panel icons,
+        // input validation foregrounds, notification icons, testing message foregrounds, peek view widget text,
+        selection: "#81577A", // +(list selected background)
+        match: "#9CA320",
+        bracket: "#5A63755A", // bracket match
+        hint: "#31A155",
+        info: "#018ACC", // +(peek view)
+        warning: "#E17615", // +(list warning foreground)
+        error: "#FF1277", // +(general error text foreground, bracket foreground mismatch, list error foreground, confusing unicode highlighting)
+
+        // Error lens line backgrounds, testing message backgrounds, peek view backgrounds,
+        // input validation backgrounds,
+        hintBg: "#293B38",
+        hintBgA: "#31A15520",
+        infoBg: "#233847",
+        infoBgA: "#018ACC20",
+        warningBg: "#403530",
+        warningBgA: "#E1761520",
+        errorBg: "#3E2A3B",
+        errorBgA: "#FF12771A",
+
+        // Testing icons,
+        testPassed: "#31A155",
+        testQueued: "#018ACC",
+        testFailed: "#FF1277",
+        testUnset: "#E17615",
+        testSkipped: "#636D83",
+    },
+
+    git: {
+        // File names, ruler/gutter/minimap decorations (where relevant),
+        addedOrStaged: "#31A155",
+        modified: "#018ACC",
+        renamed: "#13BBB7",
+        untracked: "#C75AF3",
+        removedOrConflicting: "#FF1277",
+        ignoredOrSubmodule: "#636D83",
+
+        // Diff viewer line backgrounds,
+        insertedBgA: "#31A1552A", // Entire modified line #243a31 Added #264b37
+        removedBgA: "#FF127720", // Entire modified line #3f2234 Removed #58203c
+        diffDiagonal: "#636D8388",
+
+        // Conflict viewer ruler decorations,
+        current: "#31A155",
+        incoming: "#018ACC",
+        // Conflict viewer line backgrounds,
+        currentBgA: "#31A1552A",
+        currentHeaderBgA: "#31A1556A",
+        incomingBgA: "#018ACC2A",
+        incomingHeaderBgA: "#018ACC6A",
+    },
+
+    debug: {
+        // Debug toolbar icons.
+        start: "#31A155",
+        pause: "#E17615",
+        step: "#018ACC",
+        stop: "#FF1277",
+
+        // Gutter breakpoints.
+        breakpoint: "#FF1277",
+        breakpointDisabled: "#636D83",
+
+        // Exception widget background.
+        exceptionBg: "#3D412F",
+
+
+        // Console colors.
+        info: "#10B1FE",
+        warning: "#FF6B66",
+        error: "#FF2884",
+        source: "#FFFFFF",
+        input: "#D177F5",
+    },
+
+    terminal: {
+        // Command decoration icons,
+        default: "#636D83",
+        success: "#31A155",
+        error: "#FF1277",
+
+        background: "#22252a",
+        foreground: "#ABB2BF", // 0m (foreground)
+        ansiBlack: "#fff", // 30m
+        ansiBrightBlack: "#666", // 30;1m
+        ansiWhite: "#22252a", //37m (background)
+        ansiBrightWhite: "#fff", // (technicall 37;1m but vscode also applies this to just bold 1m)
+        ansiBlue: "#09A1ED",
+        ansiBrightBlue: "#41b9ff",
+        ansiCyan: "#13BBB7",
+        ansiBrightCyan: "#16dad6",
+        ansiGreen: "#2DAE58",
+        ansiBrightGreen: "#25da6a",
+        ansiYellow: "#CF9C00",
+        ansiBrightYellow: "#ffc104",
+        ansiRed: "#ff0046",
+        ansiBrightRed: "#ff2e87",
+        ansiMagenta: "#c010ef",
+        ansiBrightMagenta: "#c75af3",
+    },
+
+    ui: {
+        // Widgets, find & replace pop-up, peek view list, inactive tab, tab header, sidebar,
+        // panel section headers, debug toolbar (+border), notification centre header, command bar,
+        // titlebar, drag prompt,
+        secondaryBg: "#1D2024",
+
+        // Sidebar section headers, widget status bar,
+        tertiaryBg: "#141619",
+
+        // Breadcrumbs, menus, any buttons which dropdown etc,
+        dropdownBg: "#22252a",
+
+        // Checkbox, dropdown (+list), text input,
+        inputBg: "#22252a",
+
+        // Editor groups, sidebar panes, panels, 
+        primaryDropBg: "#FF4C981A",
+
+        // Panel, panel section header, terminal, settings page, peek view, sidebar, text input, tab groups,
+        // dropdown, checkbox, hover widgets, exception widget, ruler, drag prompt,
+        border: "#3D434F",
+
+        // Command bar, notifications,
+        secondaryBorder: "#2f333c",
+
+        // Menu separator
+        separator: "#3D434F",
+
+        tabSeparator: "#5a6374",
+
+        // Input foreground,
+        placeholderText: "#4D5665",
+
+        codeblock: "#09A1ED",
+
+        // Normal links, notification links, text editor ctrl+click symbol,
+        link: "#FF1277",
+        linkHover: "#DA005F",
+
+        // Button background, remote status button background,
+        secondary: "#09A1ED",
+        secondaryHover: "#0784C2",
+
+        // Hover/active button overlays,
+        hoverBgA: "#0000001A",
+        activeBgA: "#0000002A",
+
+        // Hover/selected list overlay,
+        listHoverBgA: "#0000001A",
+        listInactiveBgA: "#FF4C981A",
+        // Tree indent guide,
+        treeIndent: "#3D434F",
+
+        // Scrollbar/minimap handle overlays,
+        scrollBgA: "#FFFFFF10",
+        scrollHoverBgA: "#FFFFFF1F",
+        scrollActiveBgA: "#FFFFFF2F",
+
+        // Shadow whenever some scrollable element is scrolled, shadows around widgets,
+        shadow: "#0000005F",
+
+        // Status bar during debugging,
+        statusDebugBg: "#31A155",
+        // Status bar when no folder is open,
+        statusEmptyBg: "#E5E5E5",
+        // Hover/active button overlays,
+        statusHoverBgA: "#0000002F",
+        statusActiveBgA: "#0000004F",
+
+        // Unfocused active tab indicator,
+        unfocusedTab: "#FFA2CA",
+
+        // Inactive icons,
+        activityBarInactive: "#636D83",
+
+        // Code actions icon,
+        lightBulb: "#FFC104",
+
+        // Extension badge icons,
+        star: "#FFC104",
+        remote: "#09A1ED",
+        verified: "#31A155",
+        prerelease: "#E17615",
+
+        // Chart colours,
+        chartLine: "#B9BFCA",
+        chartBlue: "#10B1FE",
+        chartGreen: "#3FC56B",
+        chartYellow: "#F9C859",
+        chartOrange: "#FF6B66",
+        chartRed: "#FF2884",
+        chartPurple: "#D177F5",
+    },
+
+    brackets: {
+        one: "#3FC56B",
+        two: "#10B1FE",
+        three: "#F9C859",
+        four: "#FF6B66",
+        five: "#D177F5",
+        six: "#B58E95",
+    },
+
+    gitGraph: [
+        "#F85EB4",
+        "#10B1FE",
+        "#3FC56B",
+        "#F9C859",
+        "#FF6B66",
+        "#D177F5",
+        "#B58E95",
+        "#97BCCD",
+        "#15C9C5",
+        "#9ACC12"
+    ],
+
+    todo: {
+        todo: "#B9BFCA",
+        fixme: "#FF6B66",
+        bug: "#FF2884",
+        hack: "#F9C859",
+        maybe: "#15C9C5",
+        unchecked: "#B9BFCA",
+        checked: "#3FC56B",
+    }
+}
+
+export const darkSyntax = {
+    keyword: "#F85EB4",
+    fn: "#10B1FE",
+    type: "#3FC56B",
+    variant: "#15C9C5",
+    constant: "#15C9C5",
+    var: "#B9BFCA",
+
+    punctuation: "#828DA0",
+    parameter: "#97BCCD",
+    member: "#B58E95",
+
+    string: "#F9C859",
+    char: "#FF6B66",
+    number: "#FF6B66",
+    format: "#D177F5",
+    comment: "#636D83",
+
+    interface: "#D177F5",
+    attribute: "#FF6B66",
+
+    invalid: "#FF2884",
+
+    rustLabel: "#9ACC12",
+    rustUnsafe: "#FF6B66",
+    rustKeyword: "#F750AE",
+    rustFn: "#01AAFA",
+    rustVar: "#ABB2BF",
+    rustParam: "#87B2C5",
+
+    jsRegex: "#B9BFCA",
+    reactEmbedded: "#FF6B66",
+    reactAttribute: "#D177F5",
+
+    psVar: "#3FC56B",
+    psSpecialVar: "#D177F5",
+    psOperator: "#F85EB4",
+    psCommentKeyword: "#B9BFCA",
+
+    htmlTag: "#F85EB4",
+    htmlAttribute: "#15C9C5",
+    htmlId: "#FF6B66",
+    htmlClass: "#3FC56B",
+    htmlLink: "#D177F5",
+    htmlMeta: "#F9C859",
+    htmlEmbeddedCss: "#9ACC12",
+
+    cssProperty: "#B58E95",
+    cssValue: "#15C9C5",
+    cssFontname: "#F9C859",
+    cssPseudoclass: "#9ACC12",
+    cssMedia: "#D177F5",
+
+    xmlNamespace: "#9ACC12",
+    xmlDoctype: "#D177F5",
+
+    mdText: "#B9BFCA",
+    mdAltText: "#636D83",
+    mdHeading: "#F85EB4",
+    mdBold: "#FF6B66",
+    mdItalic: "#10B1FE",
+    mdQuote: "#F9C859",
+    mdStrikethrough: "#636D83",
+    mdCode: "#B9BFCA",
+    mdSeparator: "#B9BFCA",
+    mdList: "#15C9C5",
+    mdUrlName: "#3FC56B",
+    mdUrl: "#D177F5",
+    mdMaths: "#9ACC12",
+    mdMathsConst: "#15C9C5",
+
+    jsonKey: "#F85EB4",
+
+    yamlKey: "#F85EB4",
+    yamlTimestamp: "#3FC56B",
+    yamlAnchor: "#9ACC12",
+
+    tomlKey: "#F85EB4",
+    tomlTimestamp: "#3FC56B",
+    tomlTable: "#9ACC12",
+    tomlArray: "#D177F5",
+
+    iniKey: "#F85EB4",
+    iniHeading: "#9ACC12",
+
+    bnfSymbol: "#3FC56B",
+    bnfBuiltin: "#15C9C5",
+}
---
+++ src/light.ts
@@ -0,0 +1,384 @@
+export const lightColors = {
+    // Note: All non-alpha backgrounds are based on `primaryBg`.
+
+    text: {
+        normal: "#565869",
+
+        // Text editor line numbers, suggestion ghost text, inactive tabs,
+        muted: "#ADB1C2",
+
+        // Breadcrumbs, general descriptions, unfocused titlebar,
+        light: "#999999",
+
+        // Status bar foreground, intellisense matching letters, intellisense selected foreground, button text,
+        // list selected text, badge foreground, command bar selected foreground, menu selected text, banner text,
+        // 
+        inverse: "#FFFFFF",
+
+        // Peek view title, peek view filenames,
+        emphasised: "#000000",
+
+        // Unnecessary text editor text,
+        faded: "#000000C0",
+
+        decoration: {
+            // Indentation guides, whitespace chars, rulers,
+            light: "#CECECE",
+            // Current indentation guide, matching bracket border,
+            dark: "#AAAAAA",
+            // Codelens
+            codelens: "#747474",
+        },
+
+        // Text editor current line, text editor folded line, hover over symbol background box,
+        currentLine: "#00000007",
+        currentLineBorder: "#00000010",
+
+        // General selection box background, input option, text editor selection boxes, terminal selection,
+        selectionBg: "#FBE3EE",
+        selectionBgA: "#FF12771A",
+        // Unfocused text editor selection boxes,
+        secondarySelectionBgA: "#FF127710",
+
+        // Tabstop box backgrounds,
+        tabstopBgA: "#FF12771A",
+
+        // Text editor & terminal highlight boxes, line background, search sidebar,
+        // peek view match highlight boxes, list filter match highlight boxes,
+        matchBg: "#F7F9CB",
+        matchBgA: "#EFF3283A",
+        matchBorder: "#11C678",
+        matchBorderA: "#11C67820",
+    },
+
+    // Cursor, editor line number, breadcrumbs focus text/background, intellisense widget matching text,
+    // intellisense widget selected background, peek view selected background, focused borders, button background,
+    // checkbox tick, list selected background, progress bar, badge background, focused tab indicator,
+    // activity bar drop border, activity bar active icon, modified setting indicator,  active panel indicator, 
+    // command bar selected background, menu selected background, border drag, banner background,
+    // status bar background, terminal cursor,
+    primary: "#FF1277",
+    primaryHover: "#DA005F",
+
+    // Text editor, gutter, minimap, breadcrumbs, peek view text editor, empty editor group, panel, terminal,
+    // active tab, hover tab, zen mode sides, activity bar, welcome page, tutorial page, notifications,
+    // drag-and-drop prompt,
+    primaryBg: "#FAFBFC",
+
+    diag: {
+        // Ruler/minimap decorations, underline squiggles, error lens foregrounds, problem panel icons,
+        // input validation foregrounds, notification icons, testing message foregrounds, peek view widget text,
+        selection: "#FBCEE2", // +(list selected background)
+        match: "#F0E657",
+        bracket: "#7777775A", // bracket match
+        hint: "#11C678",
+        info: "#00B7E4", // +(peek view)
+        warning: "#FF942F", // +(list warning foreground)
+        error: "#FF1277", // +(general error text foreground, bracket foreground mismatch, list error foreground, confusing unicode highlighting)
+
+        // Error lens line backgrounds, testing message backgrounds, peek view backgrounds,
+        // input validation backgrounds,
+        hintBg: "#DCF5EB",
+        hintBgA: "#11C67820",
+        infoBg: "#DAF3F9",
+        infoBgA: "#00B7E420",
+        warningBg: "#FBEEE1",
+        warningBgA: "#FF942F20",
+        errorBg: "#FBE3EE",
+        errorBgA: "#FF12771A",
+
+        // Testing icons,
+        testPassed: "#11C678",
+        testQueued: "#00B7E4",
+        testFailed: "#FF1277",
+        testUnset: "#FF942F",
+        testSkipped: "#ADB1C2",
+    },
+
+    git: {
+        // File names, ruler/gutter/minimap decorations (where relevant),
+        addedOrStaged: "#11C678",
+        modified: "#00B7E4",
+        renamed: "#13BBB7",
+        untracked: "#C75AF3",
+        removedOrConflicting: "#FF1277",
+        ignoredOrSubmodule: "#ADB1C2",
+
+        // Diff viewer line backgrounds,
+        insertedBgA: "#11C6782A", // Entire modified line #d3f2e6 Added #b2ead3
+        removedBgA: "#FF127720", // Entire modified line #fbddea Removed #fcc2db
+        diffDiagonal: "#ADB1C288",
+
+        // Conflict viewer ruler decorations,
+        current: "#11C678",
+        incoming: "#00B7E4",
+        // Conflict viewer line backgrounds,
+        currentBgA: "#11C6782A",
+        currentHeaderBgA: "#11C6786A",
+        incomingBgA: "#09A1ED2A",
+        incomingHeaderBgA: "#09A1ED6A",
+    },
+
+    debug: {
+        // Debug toolbar icons.
+        start: "#11C678",
+        pause: "#FF942F",
+        step: "#00B7E4",
+        stop: "#FF1277",
+
+        // Gutter breakpoints.
+        breakpoint: "#FF1277",
+        breakpointDisabled: "#ADB1C2",
+
+        // Exception widget background.
+        exceptionBg: "#F7F9CB",
+
+        // Console colors.
+        info: "#09A1ED",
+        warning: "#FF5C57",
+        error: "#FF2E87",
+        source: "#000000",
+        input: "#C75AF3",
+    },
+
+    terminal: {
+        // Command decoration icons,
+        default: "#ADB1C2",
+        success: "#11C678",
+        error: "#FF1277",
+
+        background: "#FAFBFC",
+        foreground: "#333333", // 0m (foreground)
+        ansiBlack: "#000", // 30m
+        ansiBrightBlack: "#666", // 30;1m
+        ansiWhite: "#FAFBFC", //37m (background)
+        ansiBrightWhite: "#000", // (technicall 37;1m but vscode also applies this to just bold 1m)
+        ansiBlue: "#0f7cd7",
+        ansiBrightBlue: "#09A1ED",
+        ansiCyan: "#1ea0aa",
+        ansiBrightCyan: "#13BBB7",
+        ansiGreen: "#1ea330",
+        ansiBrightGreen: "#30b95e",
+        ansiYellow: "#c07205",
+        ansiBrightYellow: "#CF9C00",
+        ansiRed: "#ff0046",
+        ansiBrightRed: "#ff3a70",
+        ansiMagenta: "#b016ee",
+        ansiBrightMagenta: "#c75af3",
+    },
+
+    ui: {
+        // Widgets, find & replace pop-up, peek view list, inactive tab, tab header, sidebar,
+        // panel section headers, debug toolbar (+border), notification centre header, command bar,
+        // titlebar, drag prompt,
+        secondaryBg: "#F3F3F3",
+
+        // Sidebar section headers, widget status bar,
+        tertiaryBg: "#E5E5E5",
+
+        // Breadcrumbs, menus, any buttons which dropdown etc,
+        dropdownBg: "#FAFBFC",
+
+        // Checkbox, dropdown (+list), text input,
+        inputBg: "#FAFBFC",
+
+        // Editor groups, sidebar panes, panels, 
+        primaryDropBg: "#FF12771A",
+
+        // Panel, panel section header, terminal, settings page, peek view, sidebar, text input, tab groups,
+        // dropdown, checkbox, hover widgets, exception widget, ruler, drag prompt,
+        border: "#CECECE",
+
+        // Command bar, notifications,
+        secondaryBorder: "#D0D1D2",
+
+        // Menu separator
+        separator: "#CECECE",
+
+        tabSeparator: "#999999",
+
+        // Input foreground,
+        placeholderText: "#CECECE",
+
+        codeblock: "#09A1ED",
+
+        // Normal links, notification links, text editor ctrl+click symbol,
+        link: "#FF1277",
+        linkHover: "#DA005F",
+
+        // Button background, remote status button background,
+        secondary: "#09A1ED",
+        secondaryHover: "#0784C2",
+
+        // Hover/active button overlays,
+        hoverBgA: "#0000001A",
+        activeBgA: "#0000002A",
+
+        // Hover/selected list overlay,
+        listHoverBgA: "#0000001A",
+        listInactiveBgA: "#FF12771A",
+        // Tree indent guide,
+        treeIndent: "#D0D1D2",
+
+        // Scrollbar/minimap handle overlays,
+        scrollBgA: "#00000010",
+        scrollHoverBgA: "#0000001F",
+        scrollActiveBgA: "#0000002F",
+
+        // Shadow whenever some scrollable element is scrolled, shadows around widgets,
+        shadow: "#0000002F",
+
+        // Status bar during debugging,
+        statusDebugBg: "#11C678",
+        // Status bar when no folder is open,
+        statusEmptyBg: "#E5E5E5",
+        // Hover/active button overlays,
+        statusHoverBgA: "#0000002F",
+        statusActiveBgA: "#0000004F",
+
+        // Unfocused active tab indicator,
+        unfocusedTab: "#FFA2CA",
+
+        // Inactive icons,
+        activityBarInactive: "#BFC0C7",
+
+        // Code actions icon,
+        lightBulb: "#FFC104",
+
+        // Extension badge icons,
+        star: "#FFC104",
+        remote: "#09A1ED",
+        verified: "#11C678",
+        prerelease: "#FF942F",
+
+        // Chart colours,
+        chartLine: "#565869",
+        chartBlue: "#09A1ED",
+        chartGreen: "#2DAE58",
+        chartYellow: "#CF9C00",
+        chartOrange: "#FF5C57",
+        chartRed: "#FF1277",
+        chartPurple: "#C75AF3",
+    },
+
+    brackets: {
+        one: "#2DAE58",
+        two: "#09A1ED",
+        three: "#CF9C00",
+        four: "#FF5C57",
+        five: "#C75AF3",
+        six: "#A8759A",
+    },
+
+    gitGraph: [
+        "#F767BB",
+        "#09A1ED",
+        "#2DAE58",
+        "#CF9C00",
+        "#FF5C57",
+        "#C75AF3",
+        "#A8759A",
+        "#6E82A6",
+        "#13BBB7",
+        "#8CBA10"
+    ],
+
+    todo: {
+        todo: "#565869",
+        fixme: "#FF5C57",
+        bug: "#FF1277",
+        hack: "#CF9C00",
+        maybe: "#13BBB7",
+        unchecked: "#565869",
+        checked: "#2DAE58",
+    }
+};
+
+export const lightSyntax = {
+    keyword: "#F767BB",
+    fn: "#09A1ED",
+    type: "#2DAE58",
+    variant: "#13BBB7",
+    constant: "#13BBB7",
+    var: "#565869",
+
+    punctuation: "#777777",
+    parameter: "#6E82A6",
+    member: "#A8759A",
+
+    string: "#CF9C00",
+    char: "#FF5C57",
+    number: "#FF5C57",
+    format: "#C75AF3",
+    comment: "#ADB1C2",
+
+    interface: "#C75AF3",
+    attribute: "#FF5C57",
+
+    invalid: "#FF1277",
+
+    rustLabel: "#8CBA10",
+    rustUnsafe: "#FF5C57",
+    rustKeyword: "#F871C0",
+    rustFn: "#1DAEf6",
+    rustVar: "#6C6E83",
+    rustParam: "#7B8dAE",
+
+    jsRegex: "#565869",
+    reactEmbedded: "#FF5C57",
+    reactAttribute: "#C75AF3",
+
+    psVar: "#2DAE58",
+    psSpecialVar: "#C75AF3",
+    psOperator: "#F767BB",
+    psCommentKeyword: "#565869",
+
+    htmlTag: "#F767BB",
+    htmlAttribute: "#13BBB7",
+    htmlId: "#FF5C57",
+    htmlClass: "#2DAE58",
+    htmlLink: "#C75AF3",
+    htmlMeta: "#CF9C00",
+    htmlEmbeddedCss: "#8CBA10",
+
+    cssProperty: "#A8759A",
+    cssValue: "#13BBB7",
+    cssFontname: "#CF9C00",
+    cssPseudoclass: "#8CBA10",
+    cssMedia: "#C75AF3",
+
+    xmlNamespace: "#8CBA10",
+    xmlDoctype: "#C75AF3",
+
+    mdText: "#565869",
+    mdAltText: "#ADB1C2",
+    mdHeading: "#F767BB",
+    mdBold: "#FF5C57",
+    mdItalic: "#09A1ED",
+    mdQuote: "#CF9C00",
+    mdStrikethrough: "#ADB1C2",
+    mdCode: "#565869",
+    mdSeparator: "#565869",
+    mdList: "#13BBB7",
+    mdUrlName: "#2DAE58",
+    mdUrl: "#C75AF3",
+    mdMaths: "#8CBA10",
+    mdMathsConst: "#13BBB7",
+
+    jsonKey: "#F767BB",
+
+    yamlKey: "#F767BB",
+    yamlTimestamp: "#2DAE58",
+    yamlAnchor: "#8CBA10",
+
+    tomlKey: "#F767BB",
+    tomlTimestamp: "#2DAE58",
+    tomlTable: "#8CBA10",
+    tomlArray: "#C75AF3",
+
+    iniKey: "#F767BB",
+    iniHeading: "#8CBA10",
+
+    bnfSymbol: "#2DAE58",
+    bnfBuiltin: "#13BBB7",
+}
